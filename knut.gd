6649ac13dc9a94d27c83c96e3e0687788e6f1db4
Stan Stanislaus <cyclinglinguist@gmail.com>
2026-08-13 14:42:34 -0500

    Fix berry bush variant scenes missing base scene reference
    Reorganized berry bush scenes into scenes/bushes/berry/, restoring
    the ext_resource + instance link the variants had lost.

--- knut.gd
+++ knut.gd
@@ -2,12 +2,23 @@ extends CharacterBody2D
 
 
 const SPEED = 60.0
-var food_need := 100
+var food_level := 100.0
 # 100 of any means death
-var toxin_levels: Dictionary[String, int] = {a = 0, b = 0, c = 0, d = 0}
+var toxin_levels: Dictionary[String, float] = {a = 0, b = 0, c = 0, d = 0}
+
+var energy := 100.0
 
 
 func _physics_process(delta: float) -> void:
+	
+	var speed_multiplier = 1
+	
+	if energy <= 33.33:
+		speed_multiplier = 0.5
+		
+		if energy <= 0:
+			food_level -= 15
+			energy = 100
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
@@ -20,15 +31,15 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var horizontal_direction := Input.get_axis("left", "right")
 	if horizontal_direction:
-		velocity.x = horizontal_direction * SPEED
+		velocity.x = horizontal_direction * SPEED * speed_multiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * speed_multiplier)
 		
 	var vertical_direction := Input.get_axis("up", "down")
 	if vertical_direction:
-		velocity.y = vertical_direction * SPEED
+		velocity.y = vertical_direction * SPEED * speed_multiplier
 	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)	
+		velocity.y = move_toward(velocity.x, 0, SPEED * speed_multiplier)	
 
 	move_and_slide()
 	
@@ -47,17 +58,27 @@ func _process(delta):
 		else:
 			$CollisionShape2D/AnimatedSprite2D.animation = "Walking"
 	
-		%FoodNeedValue.text = str(food_need)
+		%FoodNeedValue.text = str(food_level)
 		
 	
 
-
-func _on_food_need_timer_timeout() -> void:
-	food_need -= 5
-	if food_need <= 0:
+# Ideas
+# buffs and debuffs depending on where slept
+# like valheim
+func _on_vitals_timeout() -> void:
+	food_level -= 1
+	if food_level <= 0:
 		get_tree().quit()
+	
+	for key in toxin_levels:
+		if toxin_levels[key] > 0:
+			toxin_levels[key] -= 0.5
+			%ToxinLevelValue.text = str(toxin_levels)
+			
+	energy  -= 2
+	%EnergyLevelLabel.text = str(energy)
 		
-func increase_toxin_level(key: String, value: int):
+func increase_toxin_level(key: String, value: float):
 	assert(value >= 0, "increase_toxin_level expects non-negative value")
 	assert(toxin_levels.has(key))
 	toxin_levels[key] += max(value, 0)
@@ -65,3 +86,9 @@ func increase_toxin_level(key: String, value: int):
 	
 	if toxin_levels[key] >= 100:
 		get_tree().quit()
+		
+func increase_food_level(value: float):
+	assert(value >= 0, "increase_food_level expects non-negative value")
+	var safe_food_change = clamp(value, 0, 100 - food_level)
+	food_level += safe_food_change
+	
--- scenes/bushes/berry/base/berry_bush.gd
+++ scenes/bushes/berry/base/berry_bush.gd
@@ -1,6 +1,11 @@
 @tool
 extends Area2D
 
+@export var toxin_value := 66
+@export var food_value := 66
+# @export var toxin_increase_per_digest_action := 10
+# @export var toxin_decrease_per_digest_action := 20
+		
 @export var sprite_frame := 0:
 	set(value):
 		sprite_frame = value
@@ -16,7 +21,6 @@ var player_inside := false
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		print("YO")
 		player_inside = true
 	
 func _on_body_exited(body):
@@ -29,12 +33,11 @@ func _on_body_exited(body):
 func _process(_delta: float) -> void:
 	if player_inside and Input.is_action_just_pressed("interact") && sprite.frame != 0:
 		
-		%Player.food_need = 100
-		%Player/FoodNeedTimer.start()
+		%Player.increase_food_level(food_value)
+		%Player/VitalsTimer.start()
 		
 		var frame_to_toxin_array = [null, 'a', 'b', 'c', 'd']
-		var toxin_amount = 50
-		%Player.increase_toxin_level(frame_to_toxin_array[sprite.frame], toxin_amount)
+		%Player.increase_toxin_level(frame_to_toxin_array[sprite.frame], toxin_value)
 		
 		sprite.frame = 0
 
